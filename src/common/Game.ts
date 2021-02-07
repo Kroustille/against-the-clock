917cf8e30b3607258678ba2b57da7036c763484c
Sehsyha <valentin.stern@gmail.com>
2021-02-07 18:37:36 +0100

    feat: add player idle sprite

--- src/common/Game.ts
+++ src/common/Game.ts
@@ -1,5 +1,6 @@
-import { CollisionType, Color, DisplayMode, Engine } from 'excalibur'
+import { DisplayMode, Engine, Loader, Texture } from 'excalibur'
 import { Player } from '../players/Player'
+import bigDemonIdle from '../assets/images/frames/big_demon_idle_anim_f0.png'
 
 export class Game extends Engine {
   constructor() {
@@ -7,10 +8,15 @@ export class Game extends Engine {
   }
 
   public start() {
+    const txPlayer = new Texture(bigDemonIdle)
+    const loader = new Loader([txPlayer])
     const player = new Player()
 
     this.add(player)
 
-    return super.start()
+    return super.start(loader).then(() => {
+      const playerSprite = txPlayer.asSprite()
+      player.addDrawing(playerSprite)
+    })
   }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
-const path = require('path');
+const path = require('path')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
@@ -21,6 +22,18 @@ module.exports = {
             ]
           }
         }
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'assets/images'
+            }
+          }
+        ]
       }
     ]
   },
@@ -28,6 +41,7 @@ module.exports = {
     extensions: ['.ts', '.js']
   },
   plugins: [
+    new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       title: 'Against the clock',
       template: './src/index.html',
